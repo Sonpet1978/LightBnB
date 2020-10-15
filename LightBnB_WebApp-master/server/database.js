85679d4402cc54d9abf78d783dc8bd0a9dfefaab
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-10-15 12:15:33 -0400

    add sql functions

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,6 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const db = require('./index');
 
 /// Users
 
@@ -9,16 +10,23 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const query = {
+    text: `
+      SELECT * FROM users
+      WHERE email = $1;
+    `,
+    values: [email]
+  };
+
+  return new Promise((resolve, reject) => {
+    db.query(query.text, query.values, (err, res) => {
+      if (err) {
+        console.error('query error',err.stack);
+        reject(err);
+      }
+      resolve(res.rows[0]);
+    });
+  })
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -28,7 +36,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const query = {
+    text: `
+      SELECT * FROM users
+      WHERE id = $1;
+    `,
+    values: [id]
+  };
+
+  return new Promise((resolve, reject) => {
+    db.query(query.text, query.values, (err, res) => {
+      if (err) {
+        console.error('query error',err.stack);
+        reject(err);
+      }
+      resolve(res.rows[0]);
+    });
+  })
 }
 exports.getUserWithId = getUserWithId;
 
@@ -38,11 +62,26 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const addUser =  function(user) {
+    const query = {
+      text: `
+        INSERT INTO users (name, email, password)
+        VALUES
+          ($1, $2, $3) 
+        RETURNING *;
+      `,
+      values: [user.name, user.email, user.password]
+    };
+  
+    return new Promise((resolve, reject) => {
+      db.query(query.text, query.values, (err, res) => {
+        if (err) {
+          console.error('query error',err.stack);
+          reject(err);
+        }
+        resolve(res.rows);
+      });
+    })
 }
 exports.addUser = addUser;
 
@@ -54,7 +93,29 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = {
+    text: `
+    SELECT r.*, p.*, AVG (pr.rating) FROM properties p
+      LEFT OUTER JOIN reservations r ON r.property_id = p.id
+      JOIN property_reviews pr ON pr.property_id = p.id
+      WHERE r.guest_id = $1
+      GROUP BY r.id, p.id
+      HAVING r.end_date < now()::date
+      ORDER BY r.start_date DESC
+      LIMIT $2;
+    `,
+    values: [guest_id, limit]
+  };
+
+  return new Promise((resolve, reject) => {
+    db.query(query.text, query.values, (err, res) => {
+      if (err) {
+        console.error('query error',err.stack);
+        reject(err);
+      }
+      resolve(res.rows);
+    });
+  })
 }
 exports.getAllReservations = getAllReservations;
 
@@ -67,11 +128,74 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
+  const queryParams = [];
+
+  // Build necessary query statement
+  let queryString = `
+  SELECT p.*, avg(pr.rating) as average_rating
+  FROM properties p
+  LEFT OUTER JOIN property_reviews pr ON p.id = pr.property_id
+  `;
+
+  // Account for search options. Append to query based on set options, in conjunction with other options selected
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
   }
-  return Promise.resolve(limitedProperties);
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += 
+      (queryParams.length > 1)
+      ? 'AND '
+      : 'WHERE '
+    queryString += `owner_id = $${queryParams.length} `
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night, options.maximum_price_per_night);
+    queryString += 
+      (queryParams.length > 2)
+      ? 'AND '
+      : 'WHERE '
+    queryString += `
+      cost_per_night > $${queryParams.length - 1}
+      AND cost_per_night < $${queryParams.length}
+    `
+  }
+
+  queryString += `
+    GROUP BY p.id
+  `
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `
+      HAVING avg(pr.rating) > $${queryParams.length}
+    `
+  }
+
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
+
+  // Build query object then return query Promise 
+  const query = {
+    text: queryString,
+    values: queryParams
+  };
+
+  return new Promise((resolve, reject) => {
+    db.query(query.text, query.values, (err, res) => {
+      if (err) {
+        console.error('query error',err.stack);
+        reject(err);
+      }
+      resolve(res.rows);
+    });
+  })
 }
 exports.getAllProperties = getAllProperties;
 
@@ -82,9 +206,85 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let queryParams = [
+    property.owner_id, 
+    property.title,
+    property.description, 
+    property.thumbnail_photo_url, 
+    property.cover_photo_url,
+    property.cost_per_night, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code, 
+    property.country,
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms
+  ]
+  let queryStr = `
+    INSERT INTO properties 
+      (owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+        cost_per_night, street, city, province, post_code, country,
+        parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES 
+        ( $1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13, $14
+        )
+      RETURNING *;
+  `;
+
+  const query = {
+    text: queryStr,
+    values: queryParams
+  };
+
+  return new Promise((resolve, reject) => {
+    db.query(query.text, query.values, (err, res) => {
+      if (err) {
+        console.error('query error',err.stack);
+        reject(err);
+      }
+      resolve(res.rows);
+    });
+  })
 }
 exports.addProperty = addProperty;
+
+/**
+ * Add a reservation to the database
+ * @param {{}} reservation An object containing all of the reservation details.
+ * @return {Promise<{}>} A promise to the reservation.
+ */
+const addReservation = function(reservation) {
+  let queryParams = [
+    reservation.guest_id,
+    reservation.start,
+    reservation.end, 
+    reservation.property_id
+  ]
+  let queryStr = `
+    INSERT INTO reservations 
+      (guest_id, start_date, end_date, property_id)
+      VALUES 
+        ( $1, $2, $3, $4 )
+      RETURNING *;
+  `;
+
+  const query = {
+    text: queryStr,
+    values: queryParams
+  };
+
+  return new Promise((resolve, reject) => {
+    db.query(query.text, query.values, (err, res) => {
+      if (err) {
+        console.error('query error',err.stack);
+        reject(err);
+      }
+      resolve(res.rows);
+    });
+  })
+}
+exports.addReservation = addReservation;
+
+
